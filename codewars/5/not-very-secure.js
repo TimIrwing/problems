f1f80cd1f20f4bfcde50021726cfa7cfe9a786ec
Tim <timirwing@gmail.com>
2019-10-15 18:43:20 +0300

    codewars not very secure v2

--- codewars/5/not-very-secure.js
+++ codewars/5/not-very-secure.js
@@ -1,9 +1,1 @@
-function alphanumeric(string) {
-    if (!string.length) return false;
-
-    for (const letter of string) {
-        if (/[^A-Za-z0-9]/.test(letter)) return false;
-    }
-
-    return true;
-}
+const alphanumeric = (str) => /^[A-Za-z0-9]+$/.test(str);
